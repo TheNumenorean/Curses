023b48ac52d5b61b4fdda033e01b5b71dd04a726
Francesco Macagno <fjmacagno1@yahoo.com>
2011-06-13 14:28:46 -0700

    First Commit

---
+++ src/net/lotrcraft/curses/Curses.java
@@ -0,0 +1,35 @@
+package net.lotrcraft.curses;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Curses extends JavaPlugin {
+	Logger log = Logger.getLogger("minecraft");
+	Set<Player> players;
+
+	public void onDisable() {
+		log.info("Curses! Curses is disabled.");
+	}
+
+	public void onEnable() {
+		log.info("Curses! Curses is enabled.");
+		this.getServer()
+				.getPluginManager()
+				.registerEvent(Event.Type.PLAYER_MOVE, new CursesListener(),
+						Event.Priority.Highest, this);
+		players = new HashSet<Player>();
+	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+		
+		
+		return true;
+	}
+}
---
+++ src/net/lotrcraft/curses/CursesListener.java
@@ -0,0 +1,8 @@
+package net.lotrcraft.curses;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerListener;
+
+public class CursesListener extends PlayerListener{
+
+}
